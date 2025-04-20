924e970214c9e7e3b1e125aaec408f10154e633f
Ranjit Gajanan Kale <119128221+Ranjitk0001@users.noreply.github.com>
2025-04-20 14:30:39 +0530

    Intialize the project

---
+++ intershipTHS/src/main/java/com/intership/intershipTHS/IntershipThsApplication.java
@@ -0,0 +1,14 @@
+package com.intership.intershipTHS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntershipThsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntershipThsApplication.class, args);
+		System.out.println("intership THS");
+	}
+
+}
---
+++ intershipTHS/src/test/java/com/intership/intershipTHS/IntershipThsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intership.intershipTHS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntershipThsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
